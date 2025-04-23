b66be03c65f05331f495b83d31c559b818e79d19
Duc Trong Vu <88627016+dtrognn@users.noreply.github.com>
2025-04-23 14:50:27 +0700

    add KeychainWrapper.swift

---
+++ KeychainWrapper.swift
@@ -0,0 +1,142 @@
+//
+//  KeychainWrapper.swift
+//  DigoAuthentication
+//
+//  Created by dtrognn on 22/4/25.
+//
+
+import Foundation
+
+public struct KeychainWrapper {
+    private let keychainQuery: KeychainStoreQueryable
+    private let options: KeychainOptions
+
+    public init(
+        keychainQuery: GenericPasswordQueryable = GenericPasswordQueryable(service: Bundle.main.bundleIdentifier ?? "YourBundleIdentifier"),
+        options: KeychainOptions = KeychainOptions())
+    {
+        self.keychainQuery = keychainQuery
+        self.options = options
+    }
+
+    // MARK: - Write
+
+    public func write<T: Codable>(_ object: T, forKey key: String) throws {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(object)
+            try writeData(data, forKey: key)
+        } catch let error as KeychainError {
+            throw error
+        } catch {
+            throw KeychainError.encodingError(error)
+        }
+    }
+
+    public func write(_ string: String, forKey key: String) throws {
+        guard let data = string.data(using: .utf8) else {
+            throw KeychainError.stringConversionError
+        }
+        try writeData(data, forKey: key)
+    }
+
+    public func writeData(_ data: Data, forKey key: String) throws {
+        var query = baseQuery(forKey: key)
+        query[kSecAttrAccessible] = options.accessible
+        if options.synchronizable {
+            query[kSecAttrSynchronizable] = kCFBooleanTrue
+        }
+
+        /// remove existing
+        SecItemDelete(query as CFDictionary)
+
+        /// add new data
+        query[kSecValueData] = data
+        let status = SecItemAdd(query as CFDictionary, nil)
+        guard status == errSecSuccess else {
+            throw mapError(status)
+        }
+    }
+
+    // MARK: - Read
+
+    public func read<T: Codable>(_ type: T.Type, forKey key: String) throws -> T? {
+        guard let data = try readData(forKey: key) else { return nil }
+        let decoder = JSONDecoder()
+        do {
+            return try decoder.decode(type, from: data)
+        } catch {
+            throw KeychainError.decodingError(error)
+        }
+    }
+
+    public func readString(forKey key: String) throws -> String? {
+        guard let data = try readData(forKey: key) else { return nil }
+        guard let str = String(data: data, encoding: .utf8) else {
+            throw KeychainError.stringConversionError
+        }
+        return str
+    }
+
+    public func readData(forKey key: String) throws -> Data? {
+        var query = baseQuery(forKey: key)
+        query[kSecReturnData] = kCFBooleanTrue
+        query[kSecMatchLimit] = kSecMatchLimitOne
+        if options.synchronizable {
+            query[kSecAttrSynchronizable] = kCFBooleanTrue
+        }
+
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+
+        switch status {
+        case errSecSuccess:
+            guard let data = result as? Data else {
+                throw KeychainError.unexpectedStatus(status)
+            }
+            return data
+        case errSecItemNotFound:
+            return nil
+        default:
+            throw mapError(status)
+        }
+    }
+
+    // MARK: - Remove
+
+    public func remove(forKey key: String) throws {
+        let query = baseQuery(forKey: key)
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess || status == errSecItemNotFound else {
+            throw mapError(status)
+        }
+    }
+
+    public func removeAll() throws {
+        let query = keychainQuery.query
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess || status == errSecItemNotFound else {
+            throw mapError(status)
+        }
+    }
+
+    // MARK: - Helpers
+
+    private func baseQuery(forKey key: String) -> [CFString: Any] {
+        var query = keychainQuery.query
+        query[kSecAttrAccount] = key
+        return query
+    }
+
+    private func mapError(_ status: OSStatus) -> KeychainError {
+        if status == errSecItemNotFound {
+            return .itemNotFound
+        }
+        if #available(iOS 11.3, *) {
+            let message = SecCopyErrorMessageString(status, nil) as String? ?? "Unknown error"
+            return .unhandledError(message: message)
+        } else {
+            return .unexpectedStatus(status)
+        }
+    }
+}
